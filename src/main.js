c2d9df74251aea2c84f3a3c6e6e1c462525b7836
Pascal Achard <pascal.achard@gmail.com>
2021-12-17 14:47:40 +0100

    Setup Tailwind
    Add basic scss styles
    Add basic tailwind config
    Add basic layout

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './assets/css/index.css'
+
+import './assets/css/main.css'
+import './assets/scss/main.scss'
 
 createApp(App).use(store).use(router).mount('#app')
