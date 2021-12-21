9086aa13e5d8d8932bb86a1a592e07bcc941fba6
Pascal Achard <pascal.achard@gmail.com>
2021-12-21 15:55:46 +0100

    Add character infos

--- src/views/CharacterDetailView.vue
+++ src/views/CharacterDetailView.vue
@@ -1,6 +1,26 @@
 <template>
   <div>Je suis la page détail</div>
-  <p v-if="character">{{ character.name }}</p>
+  <template v-if="character">
+    <h2>{{ character.name }}</h2>
+    <img :src="character.image" :alt="`Name: ${character.name}`" />
+
+    <p>Created:</p>
+    <p>{{ created }}</p>
+    <p>Status:</p>
+    <p>{{ character.status }}</p>
+    <p>Species:</p>
+    <p>{{ character.species }}</p>
+    <p>Type:</p>
+    <p>{{ character.type }}</p>
+    <p>Gender:</p>
+    <p>{{ character.gender }}</p>
+    <p>Origin:</p>
+    <p>{{ character.origin?.name }}</p>
+    <p>location:</p>
+    <p>{{ character.location?.name }}</p>
+    <p>Episodes:</p>
+    <p>{{ episodesCount }}</p>
+  </template>
 </template>
 
 <script setup>
@@ -12,17 +32,13 @@ const store = useStore()
 const route = useRoute()
 
 // eslint-disable-next-line no-unused-vars
-// const props = defineProps({
-//   id: {
-//     type: String,
-//     required: true,
-//   },
-// })
-
-// eslint-disable-next-line no-unused-vars
 const character = computed(
   () => store.getters['charactereModule/currentCharacter']
 )
+const episodesCount = computed(() => character.value?.episode?.length)
+const created = computed(() => {
+  return new Date(character.value.created)?.toLocaleDateString('fe-FE')
+})
 
 store.dispatch('charactereModule/fetchCharacterById', { id: route.params.id })
 </script>
