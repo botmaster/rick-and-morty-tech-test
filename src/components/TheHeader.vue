345d93b07de5d9e01421af6a3273a77c48a26678
Pascal Achard <pascal.achard@gmail.com>
2021-12-22 23:33:38 +0100

    Update header look

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,18 @@
 <template>
   <header class="absolute inset-x-0 z-10 the-header p-4">
-    <nav class="relative flex justify-between p-4">
+    <nav class="relative flex justify-between items-center p-4">
       <router-link
-        class="z-10 rounded backdrop-blur bg-black/20 px-4 py-2"
+        class="z-10 backdrop-blur bg-black/20 rounded-full p-2"
         to="/"
-        >Home</router-link
-      >
+        ><img
+          width="50"
+          height="50"
+          src="~@/assets/images/Rick-and-Morty-001.svg"
+          alt=""
+      /></router-link>
       <ul class="space-x-4 flex z-10">
         <li>
-          <router-link class="bg-black/20 px-4 py-2" to="/about"
+          <router-link class="bg-black/20 px-4 py-2 rounded-lg" to="/about"
             >About</router-link
           >
         </li>
